c2d23dfd308db8169ff35426c86664ed2ba11492
Peter <pogo6348@pm.me>
2025-09-01 09:41:53 +0200

    Mediator pattern - create activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
@@ -5,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-public class ActivitiesController(IMediator mediator) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     // Some notes:
     // Updated to MEDIATOR pattern - Controllers just pass on message, no logic here
@@ -13,12 +14,18 @@ public class ActivitiesController(IMediator mediator) : BaseApiController
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await mediator.Send(new GetActivityList.Query());
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await mediator.Send(new GetActivityDetails.Query{Id = id});
+        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command{Activity = activity});
     }
 }
