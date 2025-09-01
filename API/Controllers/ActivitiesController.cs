01db0d5df2ca198d8f7894bf9e38d4406f744fef
Peter <pogo6348@pm.me>
2025-09-01 08:33:18 +0200

    Mediator pattern - update get activity controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,13 +2,14 @@ using Application.Activities.Queries;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(AppDbContext context, IMediator mediator) : BaseApiController
+public class ActivitiesController(IMediator mediator) : BaseApiController
 {
+    // Some notes:
+    // Updated to MEDIATOR pattern - Controllers just pass on message, no logic here
+    
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
@@ -18,10 +19,6 @@ public class ActivitiesController(AppDbContext context, IMediator mediator) : Ba
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        var activity = await context.Activities.FindAsync(id);
-        
-        if (activity == null) return NotFound();
-        
-        return activity;
+        return await mediator.Send(new GetActivityDetails.Query{Id = id});
     }
 }
