7ba74856ded00f310d596967f14baa4d33f32171
Peter <pogo6348@pm.me>
2025-09-01 09:57:00 +0200

    Edit activity endpoint

---
+++ Application/Activities/Commands/EditActivity.cs
@@ -0,0 +1,27 @@
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities.Commands;
+
+public class EditActivity
+{
+    public class Command : IRequest
+    {
+        public required Activity Activity { get; set; }
+    }
+
+    public class Handler(AppDbContext context) : IRequestHandler<Command>
+    {
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await context.Activities.FindAsync([request.Activity.Id], cancellationToken);
+            
+            if (activity == null) throw new Exception("Activity not found");
+
+            activity.Title = request.Activity.Title;
+            
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
