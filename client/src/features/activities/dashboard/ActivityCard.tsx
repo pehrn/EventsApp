7bcb0eab012817f77f3d6cb76ea3c95e2053588b
Peter <pogo6348@pm.me>
2025-09-26 14:48:31 +0200

    Activity detail route

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,5 +1,6 @@
 import {Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
+import { Link } from "react-router";
 
 type Props = {
     activity: Activity;
@@ -20,7 +21,7 @@ export default function ActivityCard({activity}: Props) {
             <CardActions sx={{display: 'flex', justifyContent: 'space-between', pb: 2}}>
                 <Chip label={activity.category} variant="outlined" />
                 <Box sx={{display: 'flex', gap: 1}}>
-                    <Button onClick={() => {}} size="medium" variant="contained">View</Button>
+                    <Button component={Link} to={`/activities/${activity.id}`} size="medium" variant="contained">View</Button>
                     <Button 
                         onClick={() => deleteActivity.mutate(activity.id)} 
                         disabled={deleteActivity.isPending}
