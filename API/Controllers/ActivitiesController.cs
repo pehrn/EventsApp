01eb67eeb3a72270b2def360e6c65e9689bb0fe8
Peter <pogo6348@pm.me>
2025-10-19 14:38:52 +0200

    controllers error handling

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -21,26 +21,24 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+        return HandleResult(await Mediator.Send(new GetActivityDetails.Query{Id = id}));
     }
 
     [HttpPost]
     public async Task<ActionResult<string>> CreateActivity(CreateActivityDto activityDto)
     {
-        return await Mediator.Send(new CreateActivity.Command{ActivityDto = activityDto});
+        return HandleResult(await Mediator.Send(new CreateActivity.Command{ActivityDto = activityDto}));
     }
     
     [HttpPut]
     public async Task<ActionResult> EditActivity(Activity activity)
     {
-        await Mediator.Send(new EditActivity.Command{Activity = activity});
-        return NoContent();
+        return HandleResult(await Mediator.Send(new EditActivity.Command{Activity = activity}));
     }
     
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteActivity(string id)
     {
-        await Mediator.Send(new DeleteActivity.Command{Id = id});
-        return Ok();
+        return HandleResult(await Mediator.Send(new DeleteActivity.Command{Id = id}));
     }
 }
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,5 +11,15 @@ namespace API.Controllers
         private IMediator? _mediator;
         
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>() ?? throw new InvalidOperationException("IMediator service is unavailable");
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            return result switch
+            {
+                { IsSuccess: false, Code: 404 } => NotFound(),
+                { IsSuccess: true, Value: not null } => Ok(result.Value),
+                _ => BadRequest(result.Error)
+            };
+        }
     }
 }
