3dbc4ec55b30201f21406445e63bf4157051dfa6
Peter <pogo6348@pm.me>
2025-12-27 19:06:41 +0100

    Axios interceptor - error handling

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { store } from "../stores/store";
+import { toast } from "react-toastify";
 
 const sleep = (delay: number) => new Promise(resolve => setTimeout(resolve, delay));
 
@@ -12,17 +13,37 @@ agent.interceptors.request.use(config => {
     return config;
 })
 
-agent.interceptors.response.use(async response => {
-    try {
+agent.interceptors.response.use(
+    async response => {
         await sleep(1000);
-        return response;   
-    } catch (err)
-    {
-        console.log(err);
-        return Promise.reject(err);   
-    } finally {
         store.uiStore.isIdle();
+        return response;
+    },
+    async error => {
+        await sleep(1000);
+        store.uiStore.isIdle();
+        
+        const {status} = error.response;
+        
+        switch (status) {
+            case 400:
+                toast.error('Bad request');
+                break;
+            case 401:
+                toast.error('Unauthorised');
+                break;
+            case 404:
+                toast.error('Not found');
+                break;
+            case 500:
+                toast.error('Server error');
+                break;
+            default:
+                break;
+        }
+        
+        return Promise.reject(error);
     }
-});
+);
 
 export default agent;
